0469cbd43d7a92b899a108d3650d26974ba23982
Thomas <thomas.dewit@outlook.fr>
2019-01-07 16:44:08 +0100

    accueil

---
+++ accueil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <html lang="fr">
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Projet : Back-end</title>
+    </head>
+
+    <body class="grid">
+    	<div class="contLogin">
+            <div class="contLog2">
+    		    <button id="foo" type="button" class="btn btn-lg btn-info"><span style="font-weight: bold;">Connexion</span></button>
+                <button type="button" class="btn btn-lg btn-dark"><span style="font-weight: bold;">S'inscrire</span></button>
+            </div>
+    	</div>
+        <div class="contMsg slide-in-top fade-in-top" style="font-size: 20px; color: white; font-weight: bold; margin-left: auto; margin-right: auto; margin-top: 100px;">
+            Créez et organisez votre liste de choses à faire, que ce soit pour vous ou votre projet de travail.
+        </div>
+    </body>
+</html>
